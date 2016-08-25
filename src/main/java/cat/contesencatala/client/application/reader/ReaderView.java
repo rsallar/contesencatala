ed1daf9c93933e67acfc452c4638cc4ea0dfa194
Ramon Salla <rsallar@gmail.com>
2016-08-25 13:19:43 +0200

    font size change

--- src/main/java/cat/contesencatala/client/application/reader/ReaderView.java
+++ src/main/java/cat/contesencatala/client/application/reader/ReaderView.java
@@ -4,9 +4,10 @@ import java.util.logging.Logger;
 
 import javax.inject.Inject;
 
+import com.gargoylesoftware.htmlunit.javascript.host.dom.Text;
 import com.google.gwt.dom.client.Style.Display;
+import com.google.gwt.dom.client.Style.Unit;
 import com.google.gwt.event.dom.client.ClickEvent;
-import com.google.gwt.event.dom.client.ClickHandler;
 import com.google.gwt.resources.client.ImageResource;
 import com.google.gwt.resources.client.TextResource;
 import com.google.gwt.uibinder.client.UiBinder;
@@ -20,7 +21,9 @@ import cat.contesencatala.client.application.model.Tale;
 import cat.contesencatala.client.resources.AppImagesSmall;
 import cat.contesencatala.client.resources.AppResources;
 import gwt.material.design.client.constants.FlexAlignSelf;
+import gwt.material.design.client.constants.FlexJustifyContent;
 import gwt.material.design.client.constants.IconType;
+import gwt.material.design.client.ui.MaterialButton;
 import gwt.material.design.client.ui.MaterialCardTitle;
 import gwt.material.design.client.ui.MaterialImage;
 import gwt.material.design.client.ui.MaterialLabel;
@@ -39,6 +42,8 @@ class ReaderView extends ViewWithUiHandlers<ReaderUiHandlers> implements ReaderP
     MaterialLabel text;
     @UiField
     MaterialImage image;
+    @UiField
+    MaterialButton favorite, fontplus, fontminus;
     
     /*@UiField
     SimplePanel main;*/
@@ -51,22 +56,38 @@ class ReaderView extends ViewWithUiHandlers<ReaderUiHandlers> implements ReaderP
     	this.resources = resources;
         initWidget(uiBinder.createAndBindUi(this));
         image.setFlexAlignSelf(FlexAlignSelf.CENTER);
-        title.setIconColor("red accent-2");
-        title.getIcon().addClickHandler(new ClickHandler() {
-			
-			@Override
-			public void onClick(ClickEvent arg0) {
-				 getUiHandlers().favoriteClick();
-			}
-		});
         image.getParent().getElement().getStyle().setDisplay(Display.BLOCK);
-
     }
     
     @UiHandler("back")
 	 void onClose(ClickEvent e) {
     	 this.getUiHandlers().back();
 	 }
+    
+    @UiHandler("favorite")
+	 void onFavorite(ClickEvent e) {
+    	 getUiHandlers().favoriteClick();
+	 }
+    
+    @UiHandler("fontminus")
+	void onFontLess(ClickEvent e) {
+    	double fontSize = Double.valueOf(text.getFontSize().replace("px", ""));
+    	fontplus.setEnabled(true);
+    	if(fontSize==8){
+    		fontminus.setEnabled(false);
+    	}
+    	text.setFontSize(fontSize-1, Unit.PX);
+	}
+    
+    @UiHandler("fontplus")
+   	void onFontMore(ClickEvent e) {
+    	double fontSize = Double.valueOf(text.getFontSize().replace("px", ""));
+    	fontminus.setEnabled(true);
+    	if(fontSize==28){
+    		fontplus.setEnabled(false);
+    	}
+    	text.setFontSize(fontSize+1, Unit.PX);
+   	}
     
     
 	@Override
@@ -82,8 +103,6 @@ class ReaderView extends ViewWithUiHandlers<ReaderUiHandlers> implements ReaderP
 		ImageResource imageRes = (ImageResource) bundle.getResource(tale.id+"_img");
 		
 		image.setUrl(imageRes.getSafeUri().asString());
-		
-		//image.setWidth(container.getOffsetWidth()+"px");
 		text.setText(textRes.getText());
 		title.setText(tale.title);
 				
@@ -91,13 +110,13 @@ class ReaderView extends ViewWithUiHandlers<ReaderUiHandlers> implements ReaderP
 
 	@Override
 	public void unfavorite() {
-		title.setIconType(IconType.FAVORITE_BORDER);
+		favorite.setIconType(IconType.FAVORITE_BORDER);
 
 	}
 
 	@Override
 	public void favorite() {
-		title.setIconType(IconType.FAVORITE);
+		favorite.setIconType(IconType.FAVORITE);
 
 	}
 }
